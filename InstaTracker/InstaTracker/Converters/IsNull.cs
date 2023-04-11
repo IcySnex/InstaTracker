ca9827c51337693187be53212e4d9c46bbaaea9a
IcySnex <81762750+IcySnex@users.noreply.github.com>
2023-04-11 13:14:48 +0200

    Fixed saved accounts list invisible after logging out

---
+++ InstaTracker/InstaTracker/Converters/IsNull.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Globalization;
+using Xamarin.Forms;
+
+namespace InstaTracker.Converters;
+
+class IsNull : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture) =>
+        value is null;
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) =>
+        value;
+}
--- InstaTracker/InstaTracker/Views/AccountView.xaml.cs
+++ InstaTracker/InstaTracker/Views/AccountView.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using InstaTracker.ViewModels;
 using Microsoft.Extensions.DependencyInjection;
+using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 namespace InstaTracker.Views;
@@ -10,6 +11,17 @@ public partial class AccountView : Components.Page
     public AccountView()
     {
         InitializeComponent();
-        BindingContext = App.Provider.GetRequiredService<AccountViewModel>();
+        AccountViewModel viewModel = App.Provider.GetRequiredService<AccountViewModel>();
+        BindingContext = viewModel;
+
+        // Fix for https://github.com/xamarin/Xamarin.Forms/issues/13607
+        viewModel.AccountManager.PropertyChanged += (s, e) =>
+        {
+            if (e.PropertyName != "LoggedUser" || viewModel.AccountManager.LoggedUser is not null)
+                return;
+
+            BindableLayout.SetItemsSource(SavedAccountsCollection, null);
+            BindableLayout.SetItemsSource(SavedAccountsCollection, viewModel.SavedAccounts);
+        };
     }
 }
